6f9c9d7534685351c1d91bdf8a619b6901fc9f43
Thanush19 <that.thanush.the.bloggy@gmail.com>
2023-08-13 18:43:40 +0530

    xplore page

--- client/src/pages/Xplore.jsx
+++ client/src/pages/Xplore.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SectionHead from "../Components/SectionHead";
 import SectionFlex from "../Components/SectionFlex";
 import Career1 from "/Assets/Images/Career1.svg";
@@ -10,11 +10,26 @@ import Career6 from "/Assets/Images/Career6.svg";
 import Career7 from "/Assets/Images/Career7.svg";
 import Career8 from "/Assets/Images/Career8.svg";
 import Career9 from "/Assets/Images/Career9.svg";
+import axios from "axios";
 
 function Xplore() {
+  const [xplore, setXplore] = useState([]);
+  let tag = "xplore";
+  function fetchXplore() {
+    axios
+      .get(import.meta.env.VITE_BACKEND_URL + `/api/articles?tag=${tag}`)
+      .then((res) => {
+        console.log(res.data.data);
+        setXplore(res.data.data);
+      });
+  }
+  useEffect(() => {
+    fetchXplore();
+  }, []);
+
   const head = "XPLORE";
   const body =
-    "This article dives into the common money traps that many college students fall into, and offers practical advice on how to avoid them. From overspending to getting caught up in credit card offers and Crypto scams, we’ll show you how to make wise financial choices that will set you up for success during and after college.";
+     "This article dives into the common money traps that many college students fall into, and offers practical advice on how to avoid them. From overspending to getting caught up in credit card offers and Crypto scams, we’ll show you how to make wise financial choices that will set you up for success during and after college.";
     const Image1 = [Career1, Career2, Career3];
     const Image2 = [Career4, Career5, Career6];
     const Image3 = [Career7, Career8, Career9];
@@ -33,30 +48,10 @@ function Xplore() {
   
   
     return (
-    <div>
-       <SectionHead head={head} body={body} />
+      <div>
+      <SectionHead head={head} body={body} />
       <div className="">
-        <SectionFlex
-          images={Image1}
-          texts={Text1}
-          buttons={Button1}
-          dates={Date1}
-          authors={Author1}
-        />
-        <SectionFlex
-          images={Image2}
-          texts={Text1}
-          buttons={Button1}
-          dates={Date1}
-          authors={Author1}
-        />
-        <SectionFlex
-          images={Image3}
-          texts={Text1}
-          buttons={Button1}
-          dates={Date1}
-          authors={Author1}
-        />
+        <SectionFlex images={Image1} buttons={Button1} articles={xplore} />
       </div>
     </div>
   )
