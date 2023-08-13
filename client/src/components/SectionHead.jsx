7a3b57e4702f4e58d89f9345d005d7410e2de791
Surieya <surieyasurieya@gmail.com>
2023-08-13 16:20:07 +0530

    studentHQ-Xplore-pages

--- client/src/components/SectionHead.jsx
+++ client/src/components/SectionHead.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function SectionHead(props) {
   return (
-    <div className='bg-black'>
-        <div className='p-5 lg:p-20 '>
-          <h1 className='poppins-bold text-[50px] lg:text-[100px] text-white'>{props.head}</h1>
-          <p className='poppins-regular text-2xl text-white'>{props.body}</p>
-        </div>
-        
+    <div className="bg-black">
+      <div className="p-5 lg:p-20 ">
+        <h1 className="poppins-bold text-[50px] lg:text-[100px] text-white">
+          {props.head}
+        </h1>
+        <p className="poppins-regular text-xl text-white">{props.body}</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SectionHead
+export default SectionHead;
