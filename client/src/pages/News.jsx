320b39d506a3eb85c5f90bc249d7fd7a81818a7c
HARISH-MADDMAXX <fwdtoharish@gmail.com>
2023-08-14 11:20:30 +0530

    homepage changes

--- client/src/pages/News.jsx
+++ client/src/pages/News.jsx
@@ -16,18 +16,7 @@ function News() {
     "This article dives into the common money traps that many college students fall into, and offers practical advice on how to avoid them. From overspending to getting caught up in credit card offers and Crypto scams, we’ll show you how to make wise financial choices that will set you up for success during and after college.";
 
   const Image1 = [News1, News2, News3, News4, News5];
-  const Button1 = [
-    //  "studentHQ",
-    //  "studentHQ",
-    //  "studentHQ",
-    //  "studentHQ",
-    //  "studentHQ",
-    //  "studentHQ",
-    "news",
-    "news",
-    "news",
-    "news",
-  ];
+  const Button1 = ["news", "news", "news", "news"];
   const [articles, setArticles] = useState([]);
   let tag = "news";
 
@@ -44,21 +33,6 @@ function News() {
       });
   }
 
-  // const Images = [News1, News2, News3, News4, News5];
-  // const Header = [
-  //   "LEARNING JAPANESE ",
-  //   "THE AI HYPE",
-  //   "THE CHINESE LANTERN",
-  //   "LIGHT POLLUTION",
-  //   "INSTAGRAM QUIRKS",
-  // ];
-  // const Content = [
-  //   "People learn Japanese either because they love the language or to watch anime without subtitles or perhaps to get high-paying jobs in Japan. Whatever the reason might be, here is an article which covers the ins and outs of the language and the nation, with insights from our very own Japanese instructor, Mr Kaushik.",
-  //   "People learn Japanese either because they love the language or to watch anime without subtitles or perhaps to get high-paying jobs in Japan. Whatever the reason might be, here is an article which covers the ins and outs of the language and the nation, with insights from our very own Japanese instructor, Mr Kaushik.",
-  //   "People learn Japanese either because they love the language or to watch anime without subtitles or perhaps to get high-paying jobs in Japan. Whatever the reason might be, here is an article which covers the ins and outs of the language and the nation, with insights from our very own Japanese instructor, Mr Kaushik.",
-  //   "People learn Japanese either because they love the language or to watch anime without subtitles or perhaps to get high-paying jobs in Japan. Whatever the reason might be, here is an article which covers the ins and outs of the language and the nation, with insights from our very own Japanese instructor, Mr Kaushik.",
-  //   "People learn Japanese either because they love the language or to watch anime without subtitles or perhaps to get high-paying jobs in Japan. Whatever the reason might be, here is an article which covers the ins and outs of the language and the nation, with insights from our very own Japanese instructor, Mr Kaushik.",
-  // ];
   const dates = [
     "23, March 2023",
     "23, March 2023",
@@ -71,13 +45,6 @@ function News() {
     <div>
       <SectionHead head={head} body={body} />
       <div>
-        {/* <NewsFlex
-          images={Images}
-          heads={Header}
-          contents={Content}
-          dates={Dates}
-          articles={articles}
-        /> */}
         <NewsFlex articles={articles} />
       </div>
     </div>
