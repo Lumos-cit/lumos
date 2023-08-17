48b8e3243994a6f8e9abceb486d34b7721f9b5e3
Surieya <surieyasurieya@gmail.com>
2023-08-17 13:07:27 +0530

    related-articles&mainHead

--- client/src/components/MainHead.jsx
+++ client/src/components/MainHead.jsx
@@ -2,20 +2,25 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import SectionHead from "../Components/SectionHead";
 import SectionFlex from "../Components/SectionFlex";
-import { Navigation, Pagination, Scrollbar, A11y,Autoplay  } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import './MainHead.css'
-import 'swiper/css/scrollbar';
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+} from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "./MainHead.css";
+import "swiper/css/scrollbar";
 import { Link } from "react-router-dom";
 
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 function MainHead() {
   const [cards, setCards] = useState([]);
   const [showFullContent, setShowFullContent] = useState(false);
-
 
   function fetchCards() {
     axios
@@ -26,75 +31,94 @@ function MainHead() {
       });
   }
 
-
-
   useEffect(() => {
     fetchCards();
   }, []);
 
+  console.log({ cards });
   return (
     <>
-     <Swiper
-      modules={[Navigation, Pagination, Scrollbar, A11y,Autoplay ]}
-      spaceBetween={50}
-      slidesPerView={1}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-
-  >
-    {cards && cards.map((card) => (
-      <SwiperSlide key={card.article_id} className="article-card bg-black ">
-          <div className=" bg-black w-full  ">
-            <div className="flex w-[80%] h-[25rem] justify-between mx-auto">
-              <div className="flex  text-white lg:w-1/3 lg:p-4 md:w-1/3 ">
-                <img src={card.cover_img} alt="Article Cover" className="h-full w-full" />
-              </div>
-              <div className="flex flex-col bg-black text-white w-2/3 ">
-                <div className="">
-                  <h2 className="font-bold text-3xl text-yellow-400 uppercase lg:mt-[1rem]">{card.title}</h2>
-                  <p className="font-semibold text-2xl">{card.description}</p>
-                </div>
-                <div className="font-italic text-xl max-h-[10rem] scrollbar-thin scrollbar-thumb-transparent scrollbar-track-transparent">
-                  <p className="scroll-content">
-                  {showFullContent
-              ? JSON.parse(card.content).blocks.map(block => {
-                  if (block.type === 'paragraph') {
-                    return <p>{block.data.text}</p>;
-                  }
-                  if (block.type === 'list') {
-                    return (
-                      <ul>
-                        {block.data.items.map(item => (
-                          <li>{item}</li>
-                        ))}
-                      </ul>
-                    );
-                  }
-                  return null;
-                })
-              : JSON.parse(card.content).blocks[0].data.text.substring(0, 50) + '...'}
-                  </p>
-                </div>
-                {!showFullContent && (
-                    <div className="text-yellow-500 mt-2 cursor-pointer bottom-10">
-                      <Link to={`/article/${card.article_id}`} className="text-yellow-500">
-                        Continue Reading......
-                      </Link>
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+      >
+        {cards &&
+          cards.map((card) => (
+            <SwiperSlide
+              key={card.article_id}
+              className="article-card bg-black "
+            >
+              <div className=" bg-black w-full  ">
+                <div className="flex w-[80%] h-[25rem] justify-between mx-auto">
+                  <div className="flex items-start text-white lg:w-1/3 lg:p-4 md:w-1/3 ">
+                    <img
+                      src={card.cover_img}
+                      alt="Article Cover"
+                      className="h-full w-full"
+                    />
+                  </div>
+                  <div className="flex flex-col gap-5 bg-black text-white w-2/3 ">
+                    <div className="">
+                      <h2 className="font-bold text-3xl text-yellow-400 uppercase lg:mt-[1rem]">
+                        {card.title}
+                      </h2>
+                      <p className="font-semibold text-2xl">
+                        {card.description}
+                      </p>
                     </div>
-                  )}
+                    <div className="font-italic text-xl max-h-[10rem] scrollbar-thin scrollbar-thumb-transparent scrollbar-track-transparent">
+                      <p className="scroll-content">
+                        {card.content.substring(0, 100)}
+                        <Link
+                          to={`/article/${card.article_id}`}
+                          className="text-yellow-500"
+                        >
+                          Continue Reading......
+                        </Link>
+                        {/* {showFullContent
+                          ? JSON.parse(card.content).blocks.map((block) => {
+                              if (block.type === "paragraph") {
+                                return <p>{block.data.text}</p>;
+                              }
+                              if (block.type === "list") {
+                                return (
+                                  <ul>
+                                    {block.data.items.map((item) => (
+                                      <li>{item}</li>
+                                    ))}
+                                  </ul>
+                                );
+                              }
+                              return null;
+                            })
+                          : JSON.parse(
+                              card.content
+                          ).blocks[0].data.text.substring(0, 50) + "..."
+                        } */}
+                      </p>
+                    </div>
+                    {/* {!showFullContent && (
+                      <div className="text-yellow-500 mt-2 cursor-pointer bottom-10">
+                        <Link
+                          to={`/article/${card.article_id}`}
+                          className="text-yellow-500"
+                        >
+                          Continue Reading......
+                        </Link>
+                      </div>
+                    )} */}
+                  </div>
+                </div>
               </div>
-
-            </div>
-            
-          </div>
-
-
-      </SwiperSlide>
-    ))}
-  </Swiper>
+            </SwiperSlide>
+          ))}
+      </Swiper>
     </>
   );
 }
--- client/src/pages/Article.jsx
+++ client/src/pages/Article.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import ArticlePage from "../components/ArticlePage";
 import { useParams } from "react-router-dom";
 import axios from "axios";
@@ -7,11 +7,16 @@ function Article() {
   const { articleId } = useParams();
   const [article, setArticle] = useState();
   const [author, setAuthor] = useState();
+  const [relatedArticles, setRelatedArticles] = useState([]);
+  const isMounted = useRef(false);
 
   useEffect(() => {
-    console.log(articleId);
+    // if (!isMounted.current) {
+    //   isMounted.current = true;
     fetchArticle();
-  }, []);
+    return () => setRelatedArticles((prevArticles) => []);
+    // }
+  }, [articleId]);
 
   function fetchArticle() {
     axios
@@ -21,21 +26,67 @@ function Article() {
         setArticle(res.data);
         return res;
       })
-      .then((res) => fetchAuthor(res.data.author_id));
+      .then((res) => {
+        fetchAuthor(res.data.author_id);
+        fetchRelatedArticles();
+      });
+
+    // .then((res) => fetchRelatedArticles(res.data.tags));
   }
 
   function fetchAuthor(authorId) {
     axios
       .get(import.meta.env.VITE_BACKEND_URL + "/author/" + authorId)
       .then((res) => {
-        console.log(res.data);
+        // console.log(res.data);
         setAuthor(res.data);
       });
   }
 
+  async function fetchRelatedArticles() {
+    const randomArr = getRandomNumbers();
+    console.log({ randomArr });
+    const articleData = randomArr.map(async (rnum) => {
+      const res = await axios.get(
+        import.meta.env.VITE_BACKEND_URL + `/api/article/${rnum}`
+      );
+      return res.data;
+    });
+    const resolvedArticleData = await Promise.all(articleData);
+    setRelatedArticles([...resolvedArticleData]);
+    // const randArr = getRandomNumbers()
+    // console.log("relatedArticle");
+    // const res = await axios.get(
+    //   import.meta.env.VITE_BACKEND_URL + `/api/articles?tag=${tag}`
+    // );
+    // // console.log(res.data.data);
+    // const randArr = getRandomNumbers(res.data.data);
+    // console.log(randArr);
+    // randArr.forEach((randId) => {
+    //   const foundArticle = res.data.data.find(
+    //     (article) => article.article_id === randId
+    //   );
+    //   setRelatedArticles((prevArticles) => [...prevArticles, foundArticle]);
+    // });
+  }
+
+  const getRandomNumbers = () => {
+    let tempSet = new Set();
+    while (tempSet.size < 3) {
+      //8 should be changed to 48;
+      let value = Math.floor(Math.random() * 8 + 1);
+      tempSet.add(value);
+    }
+    return [...tempSet];
+  };
+
   return (
     <div>
-      <ArticlePage data={article} author={author} />
+      <ArticlePage
+        data={article}
+        relatedArticles={relatedArticles}
+        author={author}
+      />
     </div>
   );
 }
